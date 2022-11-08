9474c0964eaeb720656c3fddf9599c974f4d39e3
イナムラ <k.inamura.sys22@morijyobi.ac.jp>
2022-11-08 12:31:31 +0900

    GITの管理対象から外しました。

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.print("身長(cm)=");
+double height = scanner.nextDouble() / 100;
+System.out.print("体重(kg)=");
+double weight = scanner.nextDouble();
+double BMI= + weight / height / height;
+System.out.println(String.format("%.2f", BMI));
+	}
+}
+
+
