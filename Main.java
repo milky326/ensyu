9b3a8f0606669949ae704eed24e225b48c28ca6e
イナムラ <k.inamura.sys22@morijyobi.ac.jp>
2022-11-08 15:15:30 +0900

    2人分計算できるように変更

--- Main.java
+++ Main.java
@@ -1,15 +1,19 @@
 import java.util.Scanner;
-public class Main {
+public class Sample {
 public static void main(String[] args) {
 Scanner scanner = new Scanner(System.in);
 System.out.print("身長(cm)=");
-double height = scanner.nextDouble() / 100;
+double height1 = scanner.nextDouble() / 100;
 System.out.print("体重(kg)=");
-double weight = scanner.nextDouble();
-double BMI= + weight / height / height;
-System.out.println(String.format("%.2f", BMI));
-
+double weight1 = scanner.nextDouble();
+double BMI1= + weight1 / height1 / height1;
+System.out.println(String.format("%.2f", BMI1));
+System.out.print("身長(cm)=");
+double height2 = scanner.nextDouble() / 100;
+System.out.print("体重(kg)=");
+double weight2 = scanner.nextDouble();
+double BMI2= + weight2 / height2 / height2;
+System.out.println(String.format("%.2f", BMI2));
 	}
 }
-
 
